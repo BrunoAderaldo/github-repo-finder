fe573a75daac171aa1c74dee1eb8287d42042ed8
Bruno Aderaldo <brunoaderaldo10@gmail.com>
2022-06-04 16:50:58 -0300

    feat: add sass options

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = (phase, { defaultConfig }) => {
+  if ("sassOptions" in defaultConfig) {
+    defaultConfig["sassOptions"] = {
+      includePaths: ["./src"],
+      prependData: `@import "src/base/styles/_base.scss";`,
+    };
+  }
+
+  return {
+    ...defaultConfig,
+    ...nextConfig,
+  };
+};
